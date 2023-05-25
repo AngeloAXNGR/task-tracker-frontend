8d2d015c6bd2291b57c21fcaab58f4ab45429aec
Angelo Santos <axngrsantos@gmail.com>
2023-05-25 10:33:00 +0800

    chore: wrap app component with a context provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,15 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
+// Context Provider
+import { ProjectContextProvider } from './contexts/ProjectContext.tsx'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ProjectContextProvider>
+        <App />
+      </ProjectContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
