9a86cd174dd5190dc7d8e9c2cf51f785df42c206
Angelo Santos <axngrsantos@gmail.com>
2023-05-25 15:34:35 +0800

    feat: conditional rendering on add project form

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,16 @@
 // Components
 import Sidebar from "../components/Sidebar"
+import AddProjectForm from "../components/AddProjectForm"
+
+// Custom Hooks
+import { useProjectFormContext } from "../hooks/useProjectFormContext"
 
 const Home = () => {
+	const {addProjectFormState} = useProjectFormContext();
 	return (
 		<div className="flex justify-between">
 			<Sidebar/>
+			{addProjectFormState && <AddProjectForm/>}
 			<div>
 				Task Bar Goes Here
 			</div>
