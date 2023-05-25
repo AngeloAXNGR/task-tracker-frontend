877a3b958fd3da851a2800e8ae3bc71408c40681
Angelo Santos <axngrsantos@gmail.com>
2023-05-25 16:26:34 +0800

    feat: create edit project form component

---
+++ src/components/EditProjectForm.tsx
@@ -0,0 +1,50 @@
+import { useProjectFormContext } from "../hooks/useProjectFormContext";
+import { useProjectContext } from "../hooks/useProjectContext";
+
+const EditProjectForm = () => {
+	const {toggleEditProjectForm, title, setTitle} = useProjectFormContext();
+	const {dispatch, activeProject} = useProjectContext();
+
+
+	const updateProject = async(e:React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		const response = await fetch(`http://localhost:4000/api/projects/${activeProject}`, {
+			method:'PATCH',
+			headers:{'Content-type': 'application/json'},
+			body: JSON.stringify({title})
+		});
+
+		const json = await response.json();
+
+		if(response.ok){
+			dispatch({type:'UPDATE_PROJECT', payload:{...json, title:title}});
+			toggleEditProjectForm(e, '', '');
+			setTitle('');
+		}
+	}
+	
+	return (
+		<div className="w-full h-screen">
+			<div className="bg-black opacity-70 w-full h-screen fixed top-0 left-0 z-0" onClick={(e) => toggleEditProjectForm(e, '', '')}></div>
+			<form action="" className="bg-white absolute mx-auto left-0 right-0 max-w-[320px] flex flex-col p-[30px] rounded-lg gap-[20px]">
+				<h1>{activeProject}</h1>
+				<h1 className="text-center text-4xl font-bold">Update Project</h1>
+				<label htmlFor="title" className="text-xl font-bold">Title:</label>
+				<input 
+					className="border-black border-2 rounded-lg py-[5px] px-[10px]"
+					type="text" 
+					id="title"
+					name="title"
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+				/>
+				<div className="flex items-center gap-[20px]">
+					<button className="px-[10px] py-[5px] rounded-md font-bold text-white bg-green-600 w-[100%] hover:bg-green-500" onClick={(e) => {updateProject(e)}}>Update</button>
+					<button className="px-[10px] py-[5px] rounded-md font-bold text-white bg-red-600 w-[100%] hover:bg-red-500" onClick={(e) => toggleEditProjectForm(e, '', '')}>Cancel</button>
+				</div>
+			</form>
+		</div>
+	)
+}
+
+export default EditProjectForm
