1c2d3b94a94d7df6902d9c1d31fe23dd3feb5916
Angelo Santos <axngrsantos@gmail.com>
2023-05-25 15:34:16 +0800

    feat: add project button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,12 +5,14 @@ import { ProjectType } from '../types/project';
 
 // Custom Hooks
 import { useProjectContext } from '../hooks/useProjectContext';
+import { useProjectFormContext } from '../hooks/useProjectFormContext';
 
 // Components
 import Project from './Project';
 
 const Sidebar = () => {
 	const {projects, dispatch} = useProjectContext();
+	const {toggleAddProjectForm} = useProjectFormContext();
 
 	useEffect(() =>{
 		const fetchProjects = async() => {
@@ -26,13 +28,12 @@ const Sidebar = () => {
 
 	},[dispatch])
 
-	console.log(projects);
-
 	return (
 		<div className="bg-slate-950 py-[10px] px-[20px] h-screen w-[320px] max-w-[100%]">
 			<h1 className="text-white font-bold text-3xl mb-[20px]">Projects</h1>
 
 			<div className="flex flex-col gap-[20px]">
+				<button className="px-[10px] py-[5px] rounded-md text-white bg-slate-700 hover:bg-slate-600 cursor-pointer items-center justify-between" onClick={(e) => toggleAddProjectForm(e)}>Add Project</button>
 				{projects &&
 					projects.map((project:ProjectType) => {
 						return(
