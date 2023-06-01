17f52026627b1bf895a036413c6fcd13f74f94b6
Angelo Santos <axngrsantos@gmail.com>
2023-06-01 19:28:19 +0800

    refactor: improve readbility of function

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,12 +1,9 @@
-const monthNames = ["Jan", "Feb", "Mar" ,"Apr" , "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-export const parseDate = (date:string) => {
-	// Separate String
-	const month = parseInt(date.split('-')[1].replace("0", ""))
-	const year = date.split('-')[0]
-	const day = date.split('-')[2].replace("0", "")
+export const parseDate = (date: string) => {
+  const [year, month, day] = date.split('-');
+  const parsedMonth = parseInt(month.replace("0", ""));
+  const parsedDay = day.replace("0", "");
 
-	
-
-	return `${day} ${monthNames[month-1]} ${year}`
-}
+  return `${parsedDay} ${monthNames[parsedMonth - 1]} ${year}`;
+};
