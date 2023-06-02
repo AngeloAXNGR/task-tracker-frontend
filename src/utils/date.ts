c6d9f990c6622e03eeedb4d8b11570b5cd853301
Angelo Santos <axngrsantos@gmail.com>
2023-06-03 00:07:03 +0800

    feat: date difference and parsedMonth fix

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,9 +1,23 @@
-const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const monthNames = [
+  "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
 
-export const parseDate = (date: string) => {
-  const [year, month, day] = date.split('-');
-  const parsedMonth = parseInt(month.replace("0", ""));
-  const parsedDay = day.replace("0", "");
+export const parseDate = (dueDate: string) => {
+  const today = new Date().toJSON().substring(0, 10);
+  const dueTimeStamp:any = new Date(today);
+  const todayTimeStamp:any = new Date(dueDate);
+  const diffTime = todayTimeStamp - dueTimeStamp;
+  const diffDay = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
-  return `${parsedDay} ${monthNames[parsedMonth - 1]} ${year}`;
+  if (diffDay === 0) {
+    return "Today";
+  } else if (diffDay === 1) {
+    return "Tomorrow";
+  } else if (diffDay === -1) {
+    return "Yesterday";
+  } else {
+    const [year, month, day] = dueDate.split('-');
+    const parsedMonth = parseInt(month);
+    return `${day} ${monthNames[parsedMonth - 1]} ${year}`;
+  }
 };
