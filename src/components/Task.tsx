0e4f11dd0ba02c4082d8bb7df5b33b81f7568753
Angelo Santos <axngrsantos@gmail.com>
2023-06-03 00:07:23 +0800

    style: update style for task components

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,5 +1,4 @@
 // React Icons
-import { BsFillTrash3Fill } from 'react-icons/bs';
 import { MdModeEditOutline } from 'react-icons/md';
 
 // Types
@@ -32,11 +31,18 @@ const Task = ({_id, title, dueDate, priority, createdAt, updatedAt}:TaskType) =>
 
 
 	return (
-		<div className="flex items-center w-[1000px] max-w-[100%] text-white text-2xl font-bold justify-center gap-[20px]">
-			<h1>{title}</h1>
-			<h1>{parseDate(dueDate)}</h1>
-			<MdModeEditOutline onClick={(e:React.MouseEvent<any>) => toggleEditTaskForm(e,_id, title, dueDate, priority)}/>
-			<BsFillTrash3Fill onClick={deleteTask}/>
+		<div className="flex items-start justify-between w-[500px] max-w-[100%] text-white">
+			<div className="flex items-start gap-[5px]">
+				<div className="border-2 h-[20px] w-[20px] rounded-full hover:bg-white cursor-pointer" onClick={deleteTask}></div>
+				<div className="flex flex-col">
+					<h1 className="mt-[-2px]">{title}</h1>
+					<h1 className={`${parseDate(dueDate) === "Yesterday" ? 'text-red-400' : parseDate(dueDate) === "Today" || parseDate(dueDate) === "Tomorrow" ? 'text-yellow-300' : 'text-white'}`}>{parseDate(dueDate)}</h1>
+				</div>
+			</div>
+
+			<div className="cursor-pointer">
+				<MdModeEditOutline onClick={(e:React.MouseEvent<any>) => toggleEditTaskForm(e,_id, title, dueDate, priority)}/>
+			</div>
 		</div>
 	)
 }
