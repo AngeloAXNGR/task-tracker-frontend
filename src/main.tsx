076aa1b4867ea7578122c69af564fe400c3893c6
Angelo Santos <axngrsantos@gmail.com>
2023-06-06 11:23:15 +0800

    chore: wrap app component with AuthContextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -8,17 +8,20 @@ import { BrowserRouter } from 'react-router-dom'
 import { ProjectContextProvider } from './contexts/ProjectContext.tsx'
 import { FormContextProvider } from './contexts/FormContext.tsx'
 import { TaskContextProvider } from './contexts/TaskContext.tsx'
+import { AuthContextProvider } from './contexts/AuthContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ProjectContextProvider>
-        <TaskContextProvider>
-          <FormContextProvider>
-            <App />
-          </FormContextProvider>
-        </TaskContextProvider>
-      </ProjectContextProvider>
+      <AuthContextProvider>
+        <ProjectContextProvider>
+          <TaskContextProvider>
+            <FormContextProvider>
+              <App />
+            </FormContextProvider>
+          </TaskContextProvider>
+        </ProjectContextProvider>
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
