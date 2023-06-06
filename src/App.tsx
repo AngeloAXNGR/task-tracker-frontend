90385cd4c4ce0a69f05ed3a9f1914ccd4519086a
Angelo Santos <axngrsantos@gmail.com>
2023-06-06 11:43:45 +0800

    feat: setup page render conditions based on login

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 // Pages
 import Home from './pages/Home'
@@ -8,17 +8,22 @@ import Header from './components/Header'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 
+// Hooks
+import useAuthContext from './hooks/useAuthContext'
 
 
 
 function App() {
+
+  const {user} = useAuthContext()
+
   return (
     <div className="bg-slate-800 h-full">
       <Header/>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/" element={user ? <Home/> : <Navigate to="/login"/>}/>
+        <Route path="/login" element={!user ? <Login/> : <Navigate to="/"/>}/>
+        <Route path="/signup" element={!user ? <Signup/> : <Navigate to="/"/>}/>
       </Routes>
     </div>
   )
