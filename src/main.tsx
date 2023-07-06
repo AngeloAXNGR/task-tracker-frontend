78cc9367d4e4abede4f4e6fe998b3245e17891d9
Angelo Santos <axngrsantos@gmail.com>
2023-07-06 11:08:29 +0800

    feat: add redux store and providers for the app

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { store } from './store/index.ts'
 
 // Context Provider
-import { ProjectContextProvider } from './contexts/ProjectContext.tsx'
 import { FormContextProvider } from './contexts/FormContext.tsx'
 import { TaskContextProvider } from './contexts/TaskContext.tsx'
 import { AuthContextProvider } from './contexts/AuthContext.tsx'
@@ -14,13 +15,13 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
-        <ProjectContextProvider>
           <TaskContextProvider>
             <FormContextProvider>
-              <App />
+              <Provider store={store}>
+                <App />
+              </Provider>
             </FormContextProvider>
           </TaskContextProvider>
-        </ProjectContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
